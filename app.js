97710857d7b49965502eec164c86dcbb91cc69ae
Allistair <194401853+Allistair-create@users.noreply.github.com>
2025-05-16 22:37:33 +0200

    Update app.js
    Signed-off-by: Allistair  <194401853+Allistair-create@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Replace with your Supabase project URL and ANON key
-const SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co';
-const SUPABASE_KEY = 'YOUR_PUBLIC_ANON_KEY';
+const SUPABASE_URL = 'https://ywbvonlcabqmmsninoao.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3YnZvbmxjYWJxbW1zbmlub2FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MjU3NTYsImV4cCI6MjA2MzAwMTc1Nn0.Xm-CsOyZDwSkyALWSZm-gFY4Zx_Ad0aE0ReSoYRUBQo';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
